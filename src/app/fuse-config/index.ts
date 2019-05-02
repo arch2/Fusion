e25f59f4f883bff61e3164412b90ffbd211f1193
Matt Stratton <mattstrattn@gmail.com>
2019-05-02 16:21:17 -0400

    f it

--- src/app/fuse-config/index.ts
+++ src/app/fuse-config/index.ts
@@ -10,34 +10,34 @@ import { FuseConfig } from '@fuse/types';
 
 export const fuseConfig: FuseConfig = {
     // Color themes can be defined in src/app/app.theme.scss
-    colorTheme      : 'theme-default',
-    customScrollbars: true,
-    layout          : {
-        style    : 'vertical-layout-1',
-        width    : 'fullwidth',
-        navbar   : {
-            primaryBackground  : 'fuse-navy-700',
-            secondaryBackground: 'fuse-navy-900',
-            folded             : false,
-            hidden             : false,
-            position           : 'left',
-            variant            : 'vertical-style-1'
+    colorTheme           : 'theme-blue-gray-dark',
+    customScrollbars     : true,
+    layout               : {
+        style      : 'vertical-layout-3',
+        width      : 'fullwidth',
+        navbar     : {
+            primaryBackground       : 'fuse-navy-700',
+            secondaryBackground     : 'fuse-navy-900',
+            folded                  : false,
+            hidden                  : false,
+            position                : 'left',
+            variant                 : 'vertical-style-1'
         },
         toolbar  : {
-            customBackgroundColor: false,
-            background           : 'fuse-white-500',
-            hidden               : false,
-            position             : 'below-static'
+            customBackgroundColor     : false,
+            background                : 'fuse-white-500',
+            hidden                    : false,
+            position                  : 'above-static'
         },
         footer   : {
-            customBackgroundColor: true,
-            background           : 'fuse-navy-900',
-            hidden               : false,
-            position             : 'below-fixed'
+            customBackgroundColor     : true,
+            background                : 'fuse-navy-900',
+            hidden                    : false,
+            position                  : 'below-fixed'
         },
         sidepanel: {
-            hidden  : false,
-            position: 'right'
+            hidden       : false,
+            position     : 'right'
         }
     }
 };
